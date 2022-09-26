91202f7679130d004cdc651cbcc6acf2bd90f06b
cosme7 <cosmewr@gmail.com>
2022-09-26 19:17:44 -0300

    Global Style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import {GlobalStyle} from "./global/styled"
 import Rotas from './services/Rotas';
 
 export default function App() {
   return (
     <div>
+      <GlobalStyle />
       <Rotas />
     </div>
   )
